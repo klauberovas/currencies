349d82f8592f58c32f1fc8749ce2674e38f3efa5
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-05 14:45:20 +0100

    Done

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,17 +1,31 @@
 import './style.css';
+import { useEffect, useState } from 'react';
 
 const currencies = {
   USD: { CZK: 23.823 },
   EUR: { CZK: 24.74 },
-  GBP: { CZK: 29.067 }
+  GBP: { CZK: 29.067 },
 };
 
-export const Rate = () => {
+export const Rate = ({ from }) => {
+  const [amount, setAmount] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://api.frankfurter.app/latest?from=${from}&to=CZK`,
+      );
+      const data = await response.json();
+      setAmount(data.rates.CZK);
+    };
+    fetchData();
+  }, [from]);
+
   return (
     <div className="rate">
-      <div className="rate__currency">1 USD</div>
+      <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value">22 CZK</div>
+      <div className="rate__value">{amount} CZK</div>
     </div>
   );
 };
